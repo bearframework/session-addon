047e98e7975599ed945c1aadbad9bccf781a11e9
Ivo Petkov <ivo@ivopetkov.com>
2021-11-29 18:57:10 +0200

    Update SessionTest.php

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -16,8 +16,32 @@ class SessionTest extends BearFramework\AddonTests\PHPUnitTestCase
     /**
      * 
      */
-    public function test1()
+    public function testBasics()
     {
-        $this->assertTrue(true);
+        $app = $this->getApp();
+
+        $app->session->set('key1', 'value1');
+
+        // Test get()
+        $value = $app->session->get('key1');
+        $this->assertEquals($value, 'value1');
+
+        // Test delete()
+        $app->session->delete('key1');
+        $value = $app->session->get('key1');
+        $this->assertEquals($value, null);
+
+        // Test deleteAll()
+        $app->session->set('key1', 'value1');
+        $app->session->set('key2', 'value2');
+        $value = $app->session->get('key1');
+        $this->assertEquals($value, 'value1');
+        $value = $app->session->get('key2');
+        $this->assertEquals($value, 'value2');
+        $app->session->deleteAll();
+        $value = $app->session->get('key1');
+        $this->assertEquals($value, null);
+        $value = $app->session->get('key2');
+        $this->assertEquals($value, null);
     }
 }
